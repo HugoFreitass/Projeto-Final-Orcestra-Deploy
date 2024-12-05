0c9a57b3d40b26921053e91170696baa705438a4
Hugo Freitas Silva <videogame041@gmail.com>
2024-12-05 18:18:19 -0300

    fix: chave api

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,12 +1,14 @@
 import axios from "axios";
 
+const apiKey = import.meta.env.API_KEY;
+
 const api = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
     headers: {
         'Content-type': 'application/json'
     },
     params: {
-        api_key: 'c0bd589456566d556432e707be797008',
+        api_key: apiKey,
         language: 'pt-BR'
     }
 });
